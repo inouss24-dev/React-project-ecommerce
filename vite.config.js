fc5400108333d93edfa1756ed45f42a74d50527f
inouss24-dev <mrinouss24@gmail.com>
2025-01-30 09:43:24 +0000

    fix navbar 2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  corePlugins: {
+    preflight: true, // S'assure que le reset CSS de Tailwind est bien activé
+  },
   plugins: [react(),tailwindcss()],
 })
